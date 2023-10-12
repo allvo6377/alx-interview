b0ba2c9f3ee70adad680a449f9cc9ccfcedb3aff
allvo6377 <alvinpilot@gmail.com>
2023-10-12 22:02:26 +0300

    copy all fixed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,26 +4,26 @@
 
 def minOperations(n):
     """
-    Calculate the minimum number of operations needed to get exactly n 'H' characters in the file.
+    Calculates the fewest number of operations needed to result in exactly n H characters in the file.
 
-    Parameters:
-    n (int): The target number of 'H' characters.
+    Args:
+        n (int): The desired number of H characters.
 
     Returns:
-    int: The minimum number of operations needed. If n is impossible to achieve, return 0.
+        int: The fewest number of operations needed to result in exactly n H characters in the file.
+             If n is impossible to achieve, returns 0.
     """
-    
-    if n == 1:
-        return 0
-
-    dp = [0] * (n + 1)
-    dp[1] = 0
-
-    for i in range(2, n + 1):
-        dp[i] = i
-        for j in range(i - 1, 0, -1):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + 2 * (i // j))
-                break
-
-    return dp[n]
+    operations = 0
+    if n <= 1:
+        return operations
+    while n > 1:
+        if n % 2 == 0:
+            n //= 2
+            operations += 2
+        else:
+            for i in range(3, n + 1, 2):
+                if n % i == 0:
+                    n //= i
+                    operations += i
+                    break
+    return operations
